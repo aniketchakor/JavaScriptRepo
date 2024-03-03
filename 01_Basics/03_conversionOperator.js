da15ad991d81d59dad7294bfba9c8e8ba873603b
aniketchakor <47598578+aniketchakor@users.noreply.github.com>
2024-03-03 13:24:38 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperator.js
@@ -0,0 +1,45 @@
+// let score = "33"
+// let score = 33
+// let score = null
+// let score = undefined
+// let score = true  
+// let score = false 
+// let score = "Aniket"
+
+/*
+console.log(typeof score); // string
+console.log(typeof(score)); // string
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber) // Number
+console.log(valueInNumber) // NaN but type is number
+*/
+
+
+// "33" => 33
+// "33abc" => NaN => type Number
+// true => 1; false => 0;
+
+// let isLoggedIn = 1 
+// let isLoggedIn = ""
+// let isLoggedIn = "Aniket"
+// let isLoggedIn = 0
+
+/*
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+*/
+
+// 1 => true; 0 => false
+// "" => false
+// "aniket" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
