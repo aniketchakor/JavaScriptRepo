5128ac9654d14c3022a7476f2740e0e18613cd8b
aniketchakor <47598578+aniketchakor@users.noreply.github.com>
2024-03-03 14:12:29 +0000

    operations on conversion

--- 01_Basics/03_conversionOperator.js
+++ 01_Basics/03_conversionOperator.js
@@ -37,9 +37,70 @@ console.log(booleanIsLoggedIn)
 // "aniket" => true
 
 
-
+/*
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
+*/
+
+
+// *************Operations************
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%2)
+
+/*
+let str1 = "Hello"
+let str2 = " Aniket"
+let str4 = str1 + str2;
+console.log(str4)
+*/
+
+/*
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log((3 + 4) * 5 % 3)
+console.log(+true)
+console.log(+"")
+*/
+
+/*
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2  // not to use like this
+*/
+
+
+/*
+let gameCounter = 100
+// gameCounter++;
+ ++gameCounter;
+console.log(gameCounter)
+*/
+
+
+// prefix and postfix
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
