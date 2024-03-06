3676cfb5196234ac77cedc258a2ad1a51404f9e1
unknown <aniketchakor@gmail.com>
2024-03-06 08:01:36 +0530

    strings first half

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,1 +1,19 @@
 console.log("New file for string")
+
+const name = "Aniket"
+const count = 20
+
+console.log(name + count);
+console.log(`my name is ${name.toUpperCase()} and count is ${count}`)
+
+
+const gameName = new String("Aniket")
+console.log(gameName[0])
+
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+
+console.log(gameName.substring(2, 4))
+
+console.log(gameName.charAt(2))
