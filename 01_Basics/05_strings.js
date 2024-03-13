396ac2b31d1d613c306d02d81a45292e8cfd4811
AniketChakor <aniketchakor@gmail.com>
2024-03-13 13:49:07 +0530

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,4 +1,5 @@
-console.log("New file for string")
+
+/*console.log("New file for string")
 
 const name = "Aniket"
 const count = 20
@@ -17,3 +18,70 @@ console.log(gameName.length)
 console.log(gameName.substring(2, 4))
 
 console.log(gameName.charAt(2))
+
+console.log("Update repo in new system")
+
+console.log(gameName.indexOf('t'))
+
+
+const newStr = gameName.substring(0, 4)
+
+console.log(newStr)
+
+const anotherStr1 = gameName.slice(3,4)
+const anotherStr = gameName.slice(-3,4)
+
+console.log(anotherStr1)
+console.log(anotherStr)
+
+const newStr1 = "    Aniket       "
+
+console.log(newStr1)
+console.log(newStr1.trim())
+
+const url = "https://aniket.com/aniket%20chakor"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('aniket'))
+
+
+const str = "My name is aniket and age is 31"
+
+const Spltstr = str.split(' ');
+console.log(Spltstr)
+
+*/
+
+//******************String Methods*****************************/
+
+// 1. String.prototype.charAt()
+
+// const sentence = "The Quick brown fox jumps over the lazy dog";
+
+// console.log(sentence.charAt(2));
+// console.log(sentence.charCodeAt(5));
+
+// const str1 = "Hello"
+// const str2 = " Aniket"
+
+// console.log(str1 + str2)
+// console.log(str1.concat(str2))
+// console.log(str1.concat(' ', str2))
+
+
+const gameName = new String("Aniket-AC")
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'))
+
+
+const newStr = gameName.substring(0, 4)
+console.log(newStr)
+
+const anthrstr = gameName.slice(-8, 4)
+console.log(anthrstr)
