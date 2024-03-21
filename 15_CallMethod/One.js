f6627f6a85b16a50a4c29c65385300f503dce97a
AniketChakor <aniketchakor@gmail.com>
2024-03-21 23:07:15 +0530

    Call Method

---
+++ 15_CallMethod/One.js
@@ -0,0 +1,17 @@
+function SetUserName(username){
+    // Complex DB Calls
+    this.username = username
+    console.log("Called");
+}
+
+function createUser(username, email, password){
+
+    SetUserName.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+
+const Obj = new createUser("Aniket", "aniket@gmail.com", "123")
+
+console.log(Obj);
